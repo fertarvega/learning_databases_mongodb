80de301141fa06d82b2fbebf2d27a18e2b5f3ab4
fertarvega <fertarvega99@gmail.com>
2021-05-26 22:32:06 -0600

    db_config file finished

---
+++ db_config.py
@@ -0,0 +1,11 @@
+""" Flask configuration to connect to the database"""
+from flask_pymongo import pymongo
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+client = pymongo.MongoClient("mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.3fmjj.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+
+db = client.db-example
